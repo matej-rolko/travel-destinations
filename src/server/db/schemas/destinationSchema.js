25871f3eeea6358b683aa8089a5a22776d4a6ff7
bartbie <bartbie37@gmail.com>
2024-09-26 01:32:28 +0200

    WIP: formatted

--- src/server/db/schemas/destinationSchema.js
+++ src/server/db/schemas/destinationSchema.js
@@ -1,26 +1,26 @@
 import mongoose from "mongoose";
 
 const destinationsSchema = new mongoose.Schema({
-  address: {
-    type: String,
-    required: true
-  },
-  country: {
-    type: String,
-    required: true
-  },
-  title: {
-    type: String,
-    required: true
-  },
-  description: {
-    type: String,
-    required: false
-  },
-  image_url: {
-    type: String,
-    required: false
-  }
+    address: {
+        type: String,
+        required: true,
+    },
+    country: {
+        type: String,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: false,
+    },
+    image_url: {
+        type: String,
+        required: false,
+    },
 });
 
 export const Destination = mongoose.model("Destination", destinationsSchema);
